78a90964ad21c746f2801adee971da3979e70a5d
Muhammad Ijaz <muhammad.bese23seecs@seecs.edu.pk>
2025-06-21 16:37:47 +0500

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+This repo contains the code for a webscraper which downloads pdfs (you must ensure that all the links returning from the beatiful soap are of the pdfs).
+
+First you use the .ipynb file to get the links.
+
+After wards in the pdfLinks variable you place the array of links from the output of the last cell in the ipynb.
+
+Then you run your backend using 
+
+npm i 
+
+npm start
+
+
+afterwards you run the html file by double clicking on it in the file explorer.
+
+Click on the download button and your pdfs will start to download one after the other
+
+
+
+
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const request = require('request');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files (our HTML)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to download PDF via proxy
+app.get('/download', (req, res) => {
+    const fileUrl = req.query.url;
+    if (!fileUrl) {
+        return res.status(400).send('Missing file URL');
+    }
+
+    console.log(fileUrl)
+
+    // Set download headers
+    res.setHeader('Content-Disposition', 'attachment; filename="case.pdf"');
+
+    // Pipe the remote PDF to the response
+    request.get(fileUrl).on('error', () => {
+        res.status(500).send('Failed to fetch file');
+    }).pipe(res);
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
